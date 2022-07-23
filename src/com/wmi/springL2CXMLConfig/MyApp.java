10a6d532f5bad4eeb452417a8230b9d82860c84c
WMI <wertanimohamedilyes@gmail.com>
2022-07-23 23:41:18 +0200

    exemple of scope in spring

--- src/com/wmi/springL2CXMLConfig/MyApp.java
+++ src/com/wmi/springL2CXMLConfig/MyApp.java
@@ -8,12 +8,13 @@ public class MyApp {
         System.out.println("begin injection");
         ClassPathXmlApplicationContext  cnx=new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        // implementation par Setter
-        coash theCoashBySetter=cnx.getBean("myImplCoach",coash.class);
 
-        // implementation par Constractor
-        coash theCoashByConstactor=cnx.getBean("myImplCoach2",coash.class);
+
+
         System.out.println("-----implementation By Constructor--------------");
+        // implementation par Constructor
+        coash theCoashByConstactor=cnx.getBean("myImplCoach2",coash.class);
+        coash theCoashByConstactor2=cnx.getBean("myImplCoach2",coash.class);
         System.out.println("==> "+theCoashByConstactor.getCoashWorkOut());
         System.out.println("==> "+theCoashByConstactor.getFortuneByXMLInjection());
 
@@ -21,7 +22,15 @@ public class MyApp {
         System.out.println("==> "+((ImplCoach2)theCoashByConstactor).getEmailAddressProperty());
         System.out.println("==> "+((ImplCoach2)theCoashByConstactor).getTeamProperty());
 
+        boolean result2 =theCoashByConstactor==theCoashByConstactor2;
+
+        System.out.println("==> the memory of theCoashByConstactor 1"+theCoashByConstactor);
+        System.out.println("==> the memory of theCoashByConstactor 2"+theCoashByConstactor2);
+
+        System.out.println("==> the result between  theCoashByConstactor 1 & 2 \t "+ result2);
         System.out.println("-----implementation By Setter--------------");
+        // implementation par Setter
+        coash theCoashBySetter=cnx.getBean("myImplCoach",coash.class);
         System.out.println("==> "+theCoashBySetter.getCoashWorkOut());
         System.out.println("==> "+theCoashBySetter.getFortuneByXMLInjection());
     }
